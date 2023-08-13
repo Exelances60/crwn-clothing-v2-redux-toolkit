cad630dba1409502e2d1506424d68f266a7a7bda
Enes Çelik <enescelik6012@gmail.com>
2023-08-13 12:50:28 +0300

    redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,39 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import logger from 'redux-logger';
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./root-reducer";
+import logger from "redux-logger";
+import { getDefaultNormalizer } from "@testing-library/react";
 
-import { rootReducer } from './root-reducer';
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middleWares),
+});
+
+/* import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
 const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
+  (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  blacklist: ['user'],
+  blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -32,3 +47,4 @@ export const store = createStore(
 );
 
 export const persistor = persistStore(store);
+ */
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,10 +1,27 @@
-import { USER_ACTION_TYPES } from './user.types';
-
+import { createSlice } from "@reduxjs/toolkit";
+/* import { USER_ACTION_TYPES } from "./user.types";
+ */
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
+export const userSlice = createSlice({
+  name: "users",
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    },
+  },
+});
+
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
+export const selectUser = (state) => state.user;
+
+/* export const userReducerOld = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
@@ -14,3 +31,4 @@ export const userReducer = (state = INITIAL_STATE, action) => {
       return state;
   }
 };
+ */
